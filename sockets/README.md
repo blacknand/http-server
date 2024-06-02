af8f11aee71326f0c02eb85a2c9c90bca132f76c
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-06-02 19:43:46 +0100

    Remove sockets dir

--- sockets/README.md
+++
@@ -1,2 +0,0 @@
-# TCP socket flow
-![TCP socket flow](https://realpython.com/cdn-cgi/image/width=769,format=auto/https://files.realpython.com/media/sockets-tcp-flow.1da426797e37.jpg)
--- sockets/echo_client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(("127.0.0.1", 8080))
-    s.sendall(b"Hello, world.")
-    data = s.recv(1024)
-
-print(f"Recieved data {data!r}")
